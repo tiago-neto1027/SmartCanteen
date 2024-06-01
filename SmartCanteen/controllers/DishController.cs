1a69c61cbeb381ebd0ea46f232498adb1632f609
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2024-06-01 09:58:50 +0100

    Dish Creation Complete

--- SmartCanteen/controllers/DishController.cs
+++ SmartCanteen/controllers/DishController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SmartCanteen.models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,19 @@ namespace SmartCanteen.controllers
 {
     internal class DishController
     {
+        public DishController() 
+        {
+        
+        }
+
+        public void AddDish(string description, DishType dishType)
+        {
+            using (var db = new SmartCanteenContext())
+            {
+                var dish = new Dish(description, dishType);
+                db.Dishes.Add(dish);
+                db.SaveChanges();
+            }
+        }
     }
 }
